4f4c1931e6c8abaea7780be88d5f871d785d7999
Jaku <mineface98@gmail.com>
2026-04-16 09:01:59 +0200

    wstepne dodanie logiki potwierdzenia email

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
     path("register/", views.register_view, name="register"),
+    path('activate/<uidb64>/<token>/', views.activate_account, name='activate'),
 ]
